a402152f299b291fe30e74db0527c12ccbe124b7
BroJoon <ydngjink1234@gmail.com>
2022-01-12 04:14:10 +0900

    delete: 초기 상태로 바꿈 다시 만들어보기

--- src/index.js
+++ src/index.js
@@ -11,25 +11,8 @@ export default function BaseballGame() {
   let ComputerInput = MissionUtils.Random.pickNumberInRange(1, 9);
 
   this.play = function (computerInputNumbers, userInputNumbers) {
-    return "결과 값 string";
+
   };
 
-  function onSubmitBtn() {
-    const isError = checkUserInput(userInput.value);
-    if (isError < 0) {
-      alert("입력값이 잘못되었습니다 !");
-      return;
-    }
-  }
-
-  function onResetBtn() {
-    ComputerInput = MissionUtils.Random.pickNumberInRange(1, 9);
-    userInput.innerHTML = "";
-    result.innerHTML = "";
-  }
-
-  onSubmitBtn.addEventListener("click", onSubmitBtn.bind(this));
-  resetBtn.addEventListener("click", onResetBtn);
 }
 
-BaseballGame();
